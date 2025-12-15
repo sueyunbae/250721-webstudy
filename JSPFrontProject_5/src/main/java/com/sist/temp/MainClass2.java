c5d6115cca922f462442eebbd27fd18b9f48305c
Sueyun Bae <sueyun8747@gmai.com>
2025-12-15 23:14:17 +0900

    11111

---
+++ JSPFrontProject_5/src/main/java/com/sist/temp/MainClass2.java
@@ -0,0 +1,37 @@
+package com.sist.temp;
+import java.lang.reflect.Method;
+import java.util.*;
+import com.sist.controller.*;
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scan=new Scanner(System.in);
+        System.out.print("URL 입력:");
+        String url=scan.next();
+        List<String> list=new ArrayList<String>();
+        list.add("com.sist.temp.Board");
+        list.add("com.sist.temp.Member");
+       for(String strName:list)
+       {
+        try
+        {
+        	Class clsName=Class.forName(strName);
+        	Object obj=clsName.getDeclaredConstructor().newInstance();
+        	
+        	// 모든 메소드 읽기 
+        	Method[] methods=clsName.getDeclaredMethods();
+        	for(Method m:methods)
+        	{
+        		RequestMapping rm=
+        				m.getAnnotation(RequestMapping.class);
+        		if(url.equals(rm.value()))
+        		{
+        			m.invoke(obj, null);
+        		}
+        	}
+        }catch(Exception ex) {}
+       }
+	}
+
+}
---
+++ JSPFrontProject_5/src/main/java/com/sist/temp/MainClass3.java
@@ -0,0 +1,27 @@
+package com.sist.temp;
+import java.util.*;
+
+import com.sist.controller.Controller;
+public class MainClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        List<String> list=new ArrayList<String>();
+        list.add("com.sist.model.BoardModel");
+        list.add("com.sist.model.SawonModel");
+        list.add("com.sist.model.MainClass");
+        list.add("com.sist.model.FoodModel");
+        try
+        {
+        	for(String cls:list)
+        	{
+        		Class clsName=Class.forName(cls);
+        		if(clsName.isAnnotationPresent(Controller.class)==false)
+        			continue;
+        		Object obj=clsName.getDeclaredConstructor().newInstance();
+        		System.out.println(obj);
+        	}
+        }catch(Exception ex){}
+	}
+
+}
